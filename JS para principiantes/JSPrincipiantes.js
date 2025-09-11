dfcb390fbc4b7e34bc22bc944ac38d13dac08480
Marco_ZT <et3138690@gmail.com>
2025-09-11 17:22:06 -0600

    Creacion de JS para principiantes
    Hice todos los pasos del tutorial enviado por el profesor para realizar todos los procesos realizados en el video

--- JS para principiantes/JSPrincipiantes.js
+++ JS para principiantes/JSPrincipiantes.js
@@ -1,4 +1,89 @@
+// despues de instalar "live server" y cliquear en go live se "vinculan" las paginas
+
+//tipos de datos
+//"hello world" //string  
+
+//100000000 //number
+
+//boolean
+//true 
+//false
+
+
+//Array
+//[Joe,Ryan, Martha]
+//[1,2,3]
+//[true, false,true,false]
+
+//object    
+{
+    //"username" 'Jhon',
+    //"score" 0.4
+    //"hours"1,
+   //"proffesional" false
+}
+
+
+var UserName= "Ryan";
+let lastname=  "caracter";
+
+UserName="Carlos";
+const PI =3.1416
 console.log("hola mundo");
 console.log("TIID-212");
 console.log("IP:192.168.117.239");
-// despues de instalar "live server" y cliquear en go live se "vinculan" las paginas
+console.log([1,2,3,4]);
+console.log({ 
+    "username": 'Ryan',
+    "score": 70.4,
+    "time":14,
+    "proffesional": true
+});
+
+console.log=("PI");
+     let number1=1248
+     let number2=3214
+     var suma= number1 + number2
+     var comp=number1>number2
+
+console.log("suma");
+console.log("comp");
+
+
+     let password= "********"
+     let imput="********"
+     let comparar= password == imput
+console.log ("comparar")
+
+let calificacion=100
+
+ if (score>90)
+ { 
+    console.log("exelente, sigue asi")
+ }else if (calificacion>60){
+   console.log("Puedes mejorar") 
+ }else if (calificacion<60){
+    console.log("Practica más")
+ }
+ let typecard ="TarjetaDebito"
+switch(typecard){
+    case TarjetaDebito:
+        console.log("es una tarjeta de debito");
+        break;
+    case (TarjetaCredito):
+        console.log("esto es una tarjeta de credito");
+        break;
+
+}
+
+//cont
+let cont = 5
+while(cont>5);{
+    console.log("hello world"); 
+    i=i-1;
+}
+
+let i count = 5
+while(count==5){
+   console.log ("hola mundo") 
+}
