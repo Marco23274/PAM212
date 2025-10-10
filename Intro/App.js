f2b2ae0816cdb9a2d1bfa18c309b4af6fda0d06c
Marco_ZT <et3138690@gmail.com>
2025-10-09 18:30:52 -0600

    Cree 3 botones y añadí un contador

--- Intro/App.js
+++ Intro/App.js
@@ -1,15 +1,26 @@
+//Import:Zona de import
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View,} from 'react-native';
+import React,{ useState } from 'react';
 
+//Main:zona de los componentes
 export default function App() {
+  const[contador,setContador]=useState(0);
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Picale ahi, veras que risas</Text>
+      <Text>Contador:{contador}</Text>
+
+      <Button title = "Sumale" onPress={()=>setContador(contador+1)} />
+      <Button title= "quitale" onPress={()=>setContador(contador-1)} /> 
+      <Button title= "reiniciar" onPress={()=>setContador(contador==0)}/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//Estilos:zona de estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
