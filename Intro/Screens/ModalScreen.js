427973a6e4a8c45eb60a57462380a72145bb5ac1
Marco_ZT <et3138690@gmail.com>
2025-11-11 22:42:36 -0600

    Edicion del archivo ModalScreen
    Se edito el archivo Modal Screen añadiendo el "ejemplo"

--- Intro/Screens/ModalScreen.js
+++ Intro/Screens/ModalScreen.js
@@ -1,14 +1,71 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import React, { useState } from 'react';
+import { Modal, View, Text, Button, StyleSheet, } from 'react-native';
 
-export default function ModalScreen(){
+export default function ModalScreen() {
+  const [modalVisible, setModalVisible] = useState(false); 
 
-    return (
-      <View>
-        <Text> Proximamente </Text>
-      </View>
-    )
-  
+  const abrirModal = () => {
+    setModalVisible(true);
+  };
+
+  const cerrarModal = () => {
+    setModalVisible(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Si presionas aqui se mostrara el modal</Text>
+
+      <Button color={"#0053c8ff"} title="Abrir Modal" onPress={abrirModal} /> 
+
+      <Modal  
+        animationType="fade"            
+        transparent={true}               
+        visible={modalVisible}           
+        onRequestClose={cerrarModal}   
+      >
+
+        <View style={styles.modalContainer}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>¡Hola! soy un Modal.</Text>
+            <Button title="Cerrar" onPress={cerrarModal} /> {/* PROP onPress: cierra modal */}
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#520fc6ff',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 30,
+  },
+  modalContainer: {
+    flex: 1,  
+    justifyContent: 'center',
+    backgroundColor: 'rgba(92, 244, 173, 0.64)',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'rgba(5, 41, 147, 0.76)',
+    borderRadius: 15,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#4a3d76ff',
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 15,
+    fontSize: 16,
+  },
+});
